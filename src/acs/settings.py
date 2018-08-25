b5c56e3d8da192b7c19b14df8aba6599067d73c2
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-25 13:59:48 -0500

    Fix app

--- src/acs/settings.py
+++ src/acs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app'
 ]
 
 ROOT_URLCONF = 'acs.urls'
