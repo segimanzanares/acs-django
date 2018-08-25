2c9125c4b61f3717514b95e622410cad9560fd4b
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-25 13:59:19 -0500

    Add user and role models

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-#
+class Role(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    deleted_at = models.DateTimeField(auto_now=False, null=True)
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    role = models.ForeignKey(Role, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    remember_token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    deleted_at = models.DateTimeField(auto_now=False, null=True)
