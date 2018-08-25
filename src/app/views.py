7e385cca85acb09fcc9897c245d9facc29bcf956
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-25 14:34:52 -0500

    Create users index route

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@require_http_methods(["GET"])
+def users_index(request):
+    return render(request, 'users/index.html')
