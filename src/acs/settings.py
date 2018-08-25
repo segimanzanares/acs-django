ce51ea4c4a2c2c35a9d226afb72bff3e6ee11815
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-25 13:55:41 -0500

    Add application

--- src/acs/settings.py
+++ src/acs/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app'
 ]
 
 ROOT_URLCONF = 'acs.urls'
