dad6efba4b805c78f1bda37e5d33c83685bb0fc3
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-25 19:31:30 -0500

    Add webpack mix file to compile assets

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.options({
+    processCssUrls: false
+});
+
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+mix.copy('node_modules/adminlte/dist/img', 'public/img');
+
+mix.js('assets/js/app.js', 'public/js')
+   .sass('assets/sass/app.scss', 'public/css');
+
