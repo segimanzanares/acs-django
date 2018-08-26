f30e23deff1e504cb6d6c54d2bcafac5805e0c34
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-25 19:29:07 -0500

    Add login view

--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+
+@require_http_methods(["GET"])
+def login(request):
+    return render(request, 'auth/login.html')
 
 @require_http_methods(["GET"])
 def users_index(request):
