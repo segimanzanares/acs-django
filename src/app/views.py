2116d6c225108c0124cd6053bf02c83399676d5b
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-28 17:28:58 -0500

    Log users into the app

--- src/app/views.py
+++ src/app/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.decorators.http import require_http_methods
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from app.models import User
 
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def login(request):
-    return render(request, 'auth/login.html')
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect(reverse('users.index'))
+        return render(request, 'auth/login.html')
+    else:
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            print("Password ok")
+            auth_login(request, user)
+            if request.GET.get('next') != None:
+                return redirect(request.GET.get('next'))
+            return redirect(reverse('users.index'))
+        else:
+            return redirect(reverse('login'))
 
 @login_required
 @require_http_methods(["GET"])
