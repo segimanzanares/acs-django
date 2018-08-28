f0f68d38f9ae3463613c9b065b9e8cab7ae4b203
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2018-08-28 17:27:36 -0500

    Add last_login to user model

--- src/app/models.py
+++ src/app/models.py
@@ -18,9 +18,15 @@ class User(models.Model):
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     remember_token = models.CharField(max_length=255, null=True)
+    last_login = models.DateTimeField(auto_now=False, null=True)
     created_at = models.DateTimeField(auto_now=True, null=False)
     updated_at = models.DateTimeField(auto_now=True, null=False)
     deleted_at = models.DateTimeField(auto_now=False, null=True)
     
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'password']
+    
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+    
+    def is_authenticated(self):
+        return True;
